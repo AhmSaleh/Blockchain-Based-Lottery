f1469ddec6c5afd629c54af79be06e11ebf64a97
SALEH <ahmedsaleh166@gmail.com>
2021-04-11 21:05:01 +0200

    first commit

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address ='0xa28c95d638464159c563B08fCeC4AE4f27453CD3';
+
+const api = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new web3.eth.Contract(api, address);
